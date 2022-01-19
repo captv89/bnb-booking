8af5e549e01f59e13a29f1a2cbab1510cde78ca9
captv89 <vishnu.raveendranathan@gmail.com>
2022-01-20 04:19:13 +0530

    added home page html

--- web/routes.go
+++ web/routes.go
@@ -25,7 +25,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	// Load static files
 	fileServer := http.FileServer(http.Dir("./static/"))
-	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
